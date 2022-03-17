f64cb5f740cd27ed7afade44ba81da0a88e1f05f
shivam patil <patilshivam456@gmail.com>
2022-03-17 09:48:08 +0530

    Updates

---
+++ precache-manifest.720455318a8035f9859bd260ed84e373.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f191f01f64b7238ca0a0",
+    "url": "/Monsters/static/css/main.f2870f4d.chunk.css"
+  },
+  {
+    "revision": "f191f01f64b7238ca0a0",
+    "url": "/Monsters/static/js/main.f191f01f.chunk.js"
+  },
+  {
+    "revision": "f99921d68d7ebc942685",
+    "url": "/Monsters/static/js/1.f99921d6.chunk.js"
+  },
+  {
+    "revision": "4933064c23f62bd41ef2",
+    "url": "/Monsters/static/js/2.4933064c.chunk.js"
+  },
+  {
+    "revision": "df263d477c470becd291",
+    "url": "/Monsters/static/js/runtime~main.df263d47.js"
+  },
+  {
+    "revision": "b87c793382b6c4d76ab86379b1debeb8",
+    "url": "/Monsters/index.html"
+  }
+];
